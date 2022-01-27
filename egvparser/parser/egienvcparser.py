e915644d03bd0ae6ef23711a9b12e191992b5754
Jaganadh Gopinadhan <jaganadhg@gmail.com>
2022-01-26 23:40:10 -0500

    Added untested logic to convert ndarray to pandas DataFrame

--- egvparser/parser/egienvcparser.py
+++ egvparser/parser/egienvcparser.py
@@ -4,19 +4,72 @@ __author__ = 'Jaganadh Gopinadhan'
 __contact__ = "https://www.linkedin.com/in/jaganadhg/"
 
 import scipy.io as sio
+import numpy as np 
+import pandas as pd
+
+
+def ndarry_todf(sensor_data : np.ndarray, colnames : list) -> pd.DataFrame:
+    """ Convert a nympy nd array to a Pandas DataFrame
+        :params sensor_data: numpy ndarray data with sensor data
+        :params colnames: column names
+        :returns sensor_frame: converted pandas dataframe
+    """
+    sensor_frame = None 
+
+    try:
+        sensor_frame = pd.DataFrame(sensor_data,
+                                    columns=colnames)
+    except:
+        print("The data is not in numpy.ndarray format")
+
+    return sensor_frame
+
+
+def data_to_df(calibration : np.ndarray,
+                        calib_names : np.ndarray,
+                        colnames : list,
+                        fault_names : np.ndarray = None) -> pd.DataFrame:
+    """ Create a pandas DataFrame from the calibration data
+        :params calibration: All calib_names sensor data as numpy.ndarray
+        :params calib_names: calibration wafer names as np.array
+        :params colnames: column names
+        :returns calib_frame: a pandas DataFrame with clibration data
+    """
+
+    calib_frame = None 
+    calib_frame_list = list()
+    
+    calib_data_range = list(range(0,calibration.shape[0]))
+
+    for idx in calib_data_range:
+        curr_df = pd.DataFrame(calibration[idx,:][0],
+                                columns = colnames)
+        curr_df['wafer_names'] = calib_names[idx]
+
+        if fault_names is not None:
+            curr_df['fault_name'] = fault_names[idx]
+
+        calib_frame_list.append(curr_df)
+
+    try:
+        calib_frame = pd.concat(calib_frame_list)
+    except:
+        print("Error in stacking the DataFrames")
+
+    return calib_frame
 
 
 def egienvec_parser(data_path : str,dkey : str = "LAMDATA") -> dict:
     """ Parse the eigenvector LAM Etch Data and return the vaues as dictionary
         Source of Data https://www.eigenvector.com/data/Etch/
         The data is in a Matlab struct file. Varibles in the files are
-        INFORMATION: [ 29x63 char]  
-        calibration: {108x1  cell}  The normal or calibration wafers
-        calib_names: [108x9  char]  Names of the calibration wafers
-        test: { 21x1  cell}  The test or faulty wafers
-        test_names: [ 21x9  char]  Names of the test wafers
-        fault_names: [ 21x9  char]  Names of the specific faults
-        variables: [ 21x14 char]  Names of the variables
+        INFORMATION: [ 29x63 char]  - 0 
+        calibration: {108x1  cell}  The normal or calibration wafers - 1
+        calib_names: [108x9  char]  Names of the calibration wafers - 2
+        test: { 21x1  cell}  The test or faulty wafers - 3
+        test_names: [ 21x9  char]  Names of the test wafers - 4
+        fault_names: [ 21x9  char]  Names of the specific faults - 5
+        variables: [ 21x14 char]  Names of the variables -6 
         :param data_apth: Path to individual .mat file
         :param dkey: Key for the data LAMDATA for MACHINE_Data.mat,
          OESDATA for OES_DATA.mat and RFMDATA for RFM_DATA.mat
